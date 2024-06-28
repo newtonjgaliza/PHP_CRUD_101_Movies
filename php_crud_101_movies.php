5207c8821488e4a6d62d1e4e3c1ff46ede942ae2
Newton Galiza <newtonjgaliza@gmail.com>
2024-06-28 11:34:26 -0700

    Add files via upload

---
+++ php_crud_101_movies.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Projeto de aplicação CRUD utilizando PDO - Agenda de Contatos
+ *
+ * Alexandre Bezerra Barbosa
+ */
+ 
+// Verificar se foi enviando dados via POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = (isset($_POST["id"]) && $_POST["id"] != null) ? $_POST["id"] : "";
+    $title = (isset($_POST["title"]) && $_POST["title"] != null) ? $_POST["title"] : "";
+    $year = (isset($_POST["year"]) && $_POST["year"] != null) ? $_POST["year"] : "";
+    
+} else if (!isset($id)) {
+    // Se não se não foi setado nenhum valor para variável $id
+    $id = (isset($_GET["id"]) && $_GET["id"] != null) ? $_GET["id"] : "";
+    $title = NULL;
+    $year = NULL;
+
+}
+ 
+// Cria a conexão com o banco de dados
+try {
+    //$conexao = new PDO("mysql:host=localhost;dbname=movies; charset=utf8", "ijbduser2", "mypassword");
+    $conexao = new PDO("mysql:host=localhost; dbname=movies;
+    charset=utf8", "ijdbuser2", "mypassword");
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexao->exec("set names utf8");
+} catch (PDOException $erro) {
+    echo "Erro na conexão:".$erro->getMessage();
+}
+ 
+// Bloco If que Salva os dados no Banco - atua como Create e Update
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $title != "") {
+    try {
+        if ($id != "") {
+            $stmt = $conexao->prepare("UPDATE Movies SET title=?, year=? WHERE id = ?");
+            $stmt->bindParam(3, $id);
+        } else {
+            $stmt = $conexao->prepare("INSERT INTO Movies (title, year) VALUES (?, ?)");
+        }
+        $stmt->bindParam(1, $title);
+        $stmt->bindParam(2, $year);
+        
+ 
+        if ($stmt->execute()) {
+            if ($stmt->rowCount() > 0) {
+                echo "Dados cadastrados com sucesso!";
+                $id = null;
+                $title = null;
+                $year = null;
+                
+            } else {
+                echo "Erro ao tentar efetivar cadastro";
+            }
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+ 
+// Bloco if que recupera as informações no formulário, etapa utilizada pelo Update
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "upd" && $id != "") {
+    try {
+        $stmt = $conexao->prepare("SELECT * FROM Movies WHERE id = ?");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            $rs = $stmt->fetch(PDO::FETCH_OBJ);
+            $id = $rs->id;
+            $title = $rs->title;
+            $year = $rs->year;
+
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+ 
+// Bloco if utilizado pela etapa Delete
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
+    try {
+        $stmt = $conexao->prepare("DELETE FROM Movies WHERE id = ?");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            echo "Registo foi excluído com êxito";
+            $id = null;
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+?>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Movies</title>
+        </head>
+        <body>
+            <form action="?act=save" method="POST" name="form1" >
+                <h1>Movies</h1>
+                <hr>
+                <input type="hidden" name="id" <?php
+                 
+                // Preenche o id no campo id com um valor "value"
+                if (isset($id) && $id != null || $id != "") {
+                    echo "value=\"{$id}\"";
+                }
+                ?> />
+                Title:
+               <input type="text" name="title" <?php
+ 
+               // Preenche o title no campo title com um valor "value"
+               if (isset($title) && $title != null || $title != "") {
+                   echo "value=\"{$title}\"";
+               }
+               ?> />
+               Year:
+               <input type="text" name="year" <?php
+ 
+               // Preenche o year no campo year com um valor "value"
+               if (isset($year) && $year != null || $year != "") {
+                   echo "value=\"{$year}\"";
+               }
+               ?> />
+              
+               <input type="submit" value="salvar" />
+               <input type="reset" value="Novo" />
+               <hr>
+            </form>
+            <table border="1" width="100%">
+                <tr>
+                    <th>Movie</th>
+                    <th>Year</th>
+                </tr>
+                <?php
+ 
+                // Bloco que realiza o papel do Read - recupera os dados e apresenta na tela
+                try {
+                    $stmt = $conexao->prepare("SELECT * FROM Movies");
+                    if ($stmt->execute()) {
+                        while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
+                            echo "<tr>";
+                            echo "<td>".$rs->title."</td><td>".$rs->year."</td><td>"
+                                       ."</td><td><center><a href=\"?act=upd&id=".$rs->id."\">[Alterar]</a>"
+                                       ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                                       ."<a href=\"?act=del&id=".$rs->id."\">[Excluir]</a></center></td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "Erro: Não foi possível recuperar os dados do banco de dados";
+                    }
+                } catch (PDOException $erro) {
+                    echo "Erro: ".$erro->getMessage();
+                }
+                ?>
+            </table>
+        </body>
+    </html>
